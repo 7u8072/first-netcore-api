c3f33e012e245d40c3d3568ee610d0946be4e0b4
Administrator <Administrator@SKY-20170609OXP>
2020-06-21 22:06:36 +0800

    init

---
+++ Controllers/HellowController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_first_api_container.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HellowController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "hellow from my first dotnet core container";
+        }
+    }
+}
